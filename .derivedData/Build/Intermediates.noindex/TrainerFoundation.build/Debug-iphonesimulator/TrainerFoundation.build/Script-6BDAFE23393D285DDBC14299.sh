3d37340e325570f0f8944d409a81c7afa59b9f0d
Васильева Наталья Валерьевна <gpbu7557@MacBookProVasilevaNV.local>
2026-06-22 12:19:39 +0300

    Remove generated local artifacts

--- .derivedData/Build/Intermediates.noindex/TrainerFoundation.build/Debug-iphonesimulator/TrainerFoundation.build/Script-6BDAFE23393D285DDBC14299.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This script configures Expo modules and generates the modules provider file.
-bash -l -c "./Pods/Target\ Support\ Files/Pods-TrainerFoundation/expo-configure-project.sh"
-
